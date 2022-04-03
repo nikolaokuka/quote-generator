ffa661074560f008d4a09b13cda142818ead4106
Nikola Okuka <nikolaokuka@gmail.com>
2022-04-03 18:14:35 +0200

    add buttons functionality for sharing and picking new quote

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+    const {text, author} = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = author;
+    }
+
+    // Check quote length to determine styling
+    if (text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = text;
 }
 
 // Get Quotes From API
@@ -19,6 +39,16 @@ async function getQuotes() {
         // Catch error here
     }
 }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes();
